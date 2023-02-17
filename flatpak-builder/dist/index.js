a49ccba8aad5c220bab2552574afd16b2cd4d8a1
tytan652 <tytan652@tytanium.xyz>
2023-02-17 11:24:32 +0100

    Add a restore-cache option

--- flatpak-builder/dist/index.js
+++ flatpak-builder/dist/index.js
@@ -226,10 +226,11 @@ const build = async (manifest, manifestPath, bundle, buildBundle, repositoryUrl,
  * @param {string} repositoryName the repository name to install the runtime from
  * @param {string} repositoryUrl the repository url
  * @param {Boolean} cacheBuildDir whether to cache the build dir or not
+ * @param {Boolean} restoreCache whether to restore the cache of the build dir or not
  * @param {string} cacheKey the default cache key if there are any
  * @returns {Promise<String>} the cacheHitKey if a cache was hit
  */
-const prepareBuild = async (repositoryName, repositoryUrl, cacheBuildDir, cacheKey) => {
+const prepareBuild = async (repositoryName, repositoryUrl, cacheBuildDir, restoreCache, cacheKey) => {
   /// If the user has set a different runtime source
   if (repositoryUrl !== 'https://flathub.org/repo/flathub.flatpakrepo') {
     await exec.exec('flatpak', ['remote-add', '--if-not-exists', repositoryName, repositoryUrl])
@@ -237,7 +238,7 @@ const prepareBuild = async (repositoryName, repositoryUrl, cacheBuildDir, cacheK
 
   // Restore the cache in case caching is enabled
   let cacheHitKey
-  if (cacheBuildDir) {
+  if (cacheBuildDir && restoreCache) {
     cacheHitKey = await cache.restoreCache(
       CACHE_PATH,
       `${cacheKey}`,
@@ -267,6 +268,7 @@ const prepareBuild = async (repositoryName, repositoryUrl, cacheBuildDir, cacheK
  * @param {string} buildDir Where to build the application
  * @param {string} localRepoName The flatpak repository name
  * @param {boolean} cacheBuildDir Whether to enable caching the build directory
+ * @param {boolean} restoreCache Whether to restore the cache or not
  * @param {string} cacheKey the default cache key if there are any
  * @param {string} arch The CPU architecture to build for
  * @param {string} mirrorScreenshotsUrl The URL to mirror screenshots
@@ -281,6 +283,7 @@ const run = async (
   buildDir,
   localRepoName,
   cacheBuildDir,
+  restoreCache,
   cacheKey,
   arch,
   mirrorScreenshotsUrl
@@ -298,7 +301,7 @@ const run = async (
 
   let cacheHitKey
   try {
-    cacheHitKey = await prepareBuild(repositoryName, repositoryUrl, cacheBuildDir, cacheKey)
+    cacheHitKey = await prepareBuild(repositoryName, repositoryUrl, cacheBuildDir, restoreCache, cacheKey)
   } catch (err) {
     core.setFailed(`Failed to prepare the build ${err}`)
   }
@@ -369,6 +372,7 @@ if (require.main === require.cache[eval('__filename')]) {
     'flatpak_app',
     'repo',
     ['y', 'yes', 'true', 'enabled', true].includes(core.getInput('cache')),
+    ['y', 'yes', 'true', 'enabled', true].includes(core.getInput('restore-cache')),
     core.getInput('cache-key'),
     core.getInput('arch'),
     core.getInput('mirror-screenshots-url')
